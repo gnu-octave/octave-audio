89cac02c9b2a291f73b8afc19d5afae67723b118
adb014 <adb014@localhost>
2007-03-07 20:20:50 +0000

    Conditional build of configure file

--- src/autogen.sh
+++ src/autogen.sh
@@ -7,6 +7,7 @@ cat configure.base >> configure.in
 cat <<EOF >> configure.in
   AC_OUTPUT(\$CONFIGURE_OUTPUTS)
   dnl XXX FIXME XXX chmod is not in autoconf's list of portable functions
+
   echo " "
   echo "  \"\\\$prefix\" is \$prefix"
   echo "  \"\\\$exec_prefix\" is \$exec_prefix"
@@ -16,5 +17,10 @@ find . -name NOINSTALL -print    # shows which toolboxes won't be installed
 ])
 EOF
 
-autoconf && rm -f configure.in
-
+autoconf configure.in > configure.tmp
+if diff configure.tmp configure >& /dev/null; then
+  rm -f configure.tmp;
+else
+  mv -f configure.tmp configure
+fi
+rm -f configure.in
