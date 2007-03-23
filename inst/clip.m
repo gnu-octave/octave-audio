be253dd896c44b38760d49993d0be7c38f1bb58d
adb014 <adb014@localhost>
2007-03-23 16:14:46 +0000

    Update the FSF address

--- inst/clip.m
+++ inst/clip.m
@@ -12,7 +12,7 @@
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 ## Clip values outside the range to the value at the boundary of the
 ## range.
