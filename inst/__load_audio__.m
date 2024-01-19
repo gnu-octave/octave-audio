068499a3c4e1ecfbf5cd65f09803d8ef80f0e30d
JohnD <john.donoghue@ieee.org>
2024-01-19 08:22:52 -0500

    * inst/__load_audio__.m, inst/__unload_audio__.m: use unix path name for doc file

--- inst/__load_audio__.m
+++ inst/__load_audio__.m
@@ -26,6 +26,7 @@ function __load_audio__ ()
   try
     pkg_dir = fileparts (fullfile (mfilename ("fullpath")));
     doc_file = fullfile (pkg_dir, "doc", "audio.qch");
+    doc_file = strrep (doc_file, '\', '/');
     if exist(doc_file, "file")
       if exist("__event_manager_register_documentation__")
         __event_manager_register_documentation__ (doc_file);
